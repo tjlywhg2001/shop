b487e9e81bd14f24edb1b4bcc3bd4bc1deb30cc4
tjlywhg2001 <tjlywhg-2001@163.com>
2019-05-12 10:09:02 +0800

    20190512 10:08

--- application/admin/controller/CatesBrands.php
+++ application/admin/controller/CatesBrands.php
@@ -7,8 +7,13 @@
 	class CatesBrands extends Controller
 	{
 		public function lst(){
+			$join =[
+				['cates c','c.cates_id = cb.cb_catesid','LEFT'],
+				['brand b','find_in_set(b.brand_id,cb.cb_brands_id)','LEFT'],
+			];
 
-			$cbList = db('cates_brands') -> alias('cb') -> field('cb.*,c.cates_id,c.cates_name') -> join('cates c','c.cates_id = cb.cb_catesid','LEFT') ->order('cb_id desc')->paginate(5);
+			$cbList = db('cates_brands') -> alias('cb') -> field('cb.*,c.cates_id,c.cates_name,GROUP_CONCAT(b.brand_name order by brand_id desc separator " + ") brand_names') -> join($join) -> order('cb_id desc')-> group('cb.cb_id') -> paginate(25);
+			// dump($cbList);die;
 			$cbBrandStr = array();
 			foreach ($cbList as $k => $v) {
 				$cbBrandIds = explode(',',$v['cb_brands_id']);
@@ -39,7 +44,7 @@
 			$cates = db('cates') -> where(array('cates_pid' => 0)) -> select();
 
 			// 品牌
-			$brands = db('brand') -> select();
+			$brands = db('brand') -> where('brand_img','NEQ','') -> select();
 			$this -> assign([
 				'brands' => $brands,
 				'cates' => $cates,
@@ -103,7 +108,8 @@
 			$cates = db('cates') -> where(array('cates_pid' => 0)) -> select();
 
 			// 品牌
-			$brands = db('brand') -> select();
+			$brands = db('brand') -> where('brand_img','NEQ','') -> select();
+			dump($brands);die;
 			$this -> assign([
 				'cbList' => $cbList,
 				'brands' => $brands,
